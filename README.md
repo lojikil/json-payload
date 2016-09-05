48ee915e2eb72fdad52d566c7fafbdd567298d0e
Stefan Edwards <saedwards.ecc@gmail.com>
2016-09-05 14:53:51 -0400

    initial commit of gitignore, updated readme, added better parser of arguments

--- README.md
+++ README.md
@@ -16,6 +16,14 @@ The output is relatively simple:
 - the third is the length of the encoded payload
 - the fourth is the percent-encoded payload
 
+A more complex example is:
+
+    $ ./json-payload 'test:"test this thing"' 'foo:"bar baz blah"' foo2 '"this is a key test":"test value"' foo3:bar3
+    105
+    {"foo":"bar baz blah","foo2":"","foo3":"bar3","test":"test this thing","this is a key test":"test value"}
+    167
+    %7B%22foo%22%3A%22bar+baz+blah%22%2C%22foo2%22%3A%22%22%2C%22foo3%22%3A%22bar3%22%2C%22test%22%3A%22test+this+thing%22%2C%22this+is+a+key+test%22%3A%22test+value%22%7D
+
 # Ideas
 
 - arrays: `foo:[1,2,3,4,5]`, or `foo:[1,2,3,4,5]:int`
--- json-payload.go
+++ json-payload.go
@@ -5,9 +5,24 @@ import (
     "encoding/json"
     "net/url"
     "os"
-    "strings"
-    "strconv"
+    //"strings"
+    //"strconv"
 )
+
+const (
+    Start int = iota
+    Name // a raw name like 'test'
+    NameStr // a name inside string quotes '"test"'
+    Value // a raw value like 'test'
+    ValArr // an array of values, not nested
+    ValStr // a quoted string value
+    ValTransfer
+    TypeTransfer
+    TypeDec
+    End
+    Error
+)
+
 
 /* TODO:
  * - a proper parser, so that we can escape ':' in strings
@@ -15,12 +30,124 @@ import (
  * - `multi.level.objects=foo`
  */
 
+func parse(src string) (string, interface{}, bool) {
+    /* we return the:
+     * - name
+     * - value
+     * as a properly parsed structure
+     */
+    state := Start
+    offset := 0
+    svalue := make([]byte, 256)
+    validx := 0
+    var name string
+    var retval interface{}
+    for idx := 0; idx < len(src); idx++ {
+        switch state {
+            case Start:
+                if  src[idx] == '"' {
+                    state = NameStr
+                } else {
+                    state = Name
+                }
+            case Name:
+                if src[idx] == ':' {
+                    state = ValTransfer
+                    name = src[0:idx]
+                } else if idx + 1 >= len(src) {
+                    retval = ""
+                    name = src[0:idx+1]
+                    state = End
+                }
+            case NameStr:
+                /* can probably collapse the two bare-string and quoted-string
+                 * states into one with a little book keeping.
+                 */
+                if src[idx] == '\\' {
+                    if src[idx + 1] == 'n' {
+                        svalue[validx] = '\n'
+                    } else if src[idx + 1] == 'r' {
+                        svalue[validx] = '\r'
+                    } else if src[idx + 1] == 't' {
+                        svalue[validx] = '\t'
+                    } else if src[idx + 1] == '\\' {
+                        svalue[validx] = '"'
+                    }
+                    idx++
+                } else if src[idx] == '"' {
+                    name = string(svalue[0:validx])
+                    validx = -1
+                    if idx + 1 >= len(src) {
+                        state = End
+                    } else {
+                        state = ValTransfer
+                    }
+                } else {
+                    svalue[validx] = src[idx]
+                }
+                validx++
+            case Value:
+                retval = string(src[offset:idx + 1])
+                if src[idx] == ':' {
+                    state = TypeTransfer
+                } else if idx + 1 >= len(src) {
+                    state = End
+                }
+            case ValTransfer:
+                if src[idx] == '"' {
+                    state = ValStr
+                } else if src[idx] == '[' {
+                    state = ValArr
+                } else if src[idx] == ':' {
+                    state = ValTransfer
+                } else {
+                    offset = idx
+                    state = Value
+                }
+            case ValStr:
+                if src[idx] == '\\' {
+                    if src[idx + 1] == 'n' {
+                        svalue[validx] = '\n'
+                    } else if src[idx + 1] == 'r' {
+                        svalue[validx] = '\r'
+                    } else if src[idx + 1] == 't' {
+                        svalue[validx] = '\t'
+                    } else if src[idx + 1] == '\\' {
+                        svalue[validx] = '"'
+                    }
+                    idx++
+                } else if src[idx] == '"' {
+                    retval = string(svalue[0:validx])
+                    if idx + 1 >= len(src) {
+                        state = End
+                    } else {
+                        state = TypeTransfer
+                    }
+                } else {
+                    svalue[validx] = src[idx]
+                }
+                validx++
+            case TypeTransfer:
+                if idx + 1 >= len(src) {
+                    state = End
+                } else if src[idx] == ':' {
+                    state = TypeDec
+                } else {
+                    state = Error
+                }
+            case Error:
+                return name, nil, true
+        }
+    }
+    return name, retval, !(state == End)
+}
+
 func main() {
     res :=  make(map[string] interface{})
     args := os.Args[1:]
 
     for i := 0; i < len(args); i++ {
-        vals := strings.Split(args[i], ":")
+        /*vals := strings.Split(args[i], ":")
         l := len(vals);
         if l == 1 {
             res[args[i]] = ""
@@ -50,6 +177,10 @@ func main() {
             fmt.Println("name with value & type")
         } else {
             fmt.Println("error");
+        }*/
+        name, value, err := parse(args[i])
+        if !err {
+            res[name] = value
         }
     }
 
